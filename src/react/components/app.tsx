770175b6095c410eb7bae32e2f8b98802f6ae79e
Jason <acinate@hotmail.com>
2020-08-10 19:47:43 -0400

    updated styles

--- src/react/components/app.tsx
+++ src/react/components/app.tsx
@@ -1,18 +1,18 @@
-import {hot} from 'react-hot-loader/root';
-import React from 'react';
-import 'assets/scss/app.scss';
+import {hot} from "react-hot-loader/root";
+import React from "react";
+import "assets/scss/app.scss";
 
 import logo from "assets/img/react.png";
 
 const App = () => {
-    return (
-        <div className="app_container">
-            <div className="app_inner">
-                <h1>Welcome to React-Typescript!</h1>
-                <img src={logo} alt="react_logo"/>
-            </div>
-        </div>
-    )
+  return (
+    <div className="app_container">
+      <div className="app_inner">
+        <h1 id="h1_welcome">Welcome to React-Typescript!</h1>
+        <img src={logo} alt="react_logo"/>
+      </div>
+    </div>
+  )
 };
 
 export default hot(App);
